be304dd0c7e59ebb7e9b0a8052275fffacbbcd50
juanpabloacosta <acosta.jp@icloud.com>
2022-11-19 18:33:19 -0500

    Removed malformed profile usecase file

--- src/test/java/profile/UserProfileInteractorTest.java
+++ src/test/java/profile/UserProfileInteractorTest.java
@@ -16,29 +16,12 @@ class UserProfileInteractorTest {
 
     @Test
     void create() {
-        // To test the use case:
-        // 1) Create a UserRegisterInteractor and prerequisite objects
-        //    (arguments for the UserRegisterInteractor constructor parameters)
-        // 2) create the Input Data
-        // 3) Call the use case User Input Boundary method to run the use case
-        // 4) Check that the Output Data passed to the Presenter is correct
-        // 5) Check that the expected changes to the data layer are there.
-
-        // 1) UserRegisterInteractor and prerequisite objects
-        // We're going to need a place to save and look up information. We could
-        // use FileUser, but because unit tests are supposed to be independent
-        // that would make us also reset the file when we are done.
-        // Instead, we're going to "mock" that info using a short-lived solution
-        // that just keeps the info in a dictionary, and it won't be persistent.
-        // (Separately, elsewhere, we will need to test the FileUser works
-        // properly.)
         UserProfileDsGateway userRepository = new InMemoryUser();
 
-        // This creates an anonymous implementing class for the Output Boundary.
         UserProfilePresenter presenter = new UserProfilePresenter() {
             @Override
             public UserProfileResponseModel prepareSuccessView(UserProfileResponseModel user) {
-                // 4) Check that the Output Data and associated changes
+                // Check that the Output Data and associated changes
                 // are correct
                 assertEquals("jpablo2002", user.getName());
                 assertNotNull(user.getJoinDate()); // any creation time is fine.
@@ -60,12 +43,9 @@ class UserProfileInteractorTest {
         UserProfileInputBoundary interactor = new UserProfileInteractor(
                 userRepository, presenter);
 
-        // 2) Input data — we can make this up for the test. Normally it would
-        // be created by the Controller.
         UserProfileRequestModel inputData = new UserProfileRequestModel(
                 "1912");
 
-        // 3) Run the use case
         interactor.create(inputData);
     }
 
