027d26d5f0929985829f5f3f0b11792f3e845fe8
juanpabloacosta <acosta.jp@icloud.com>
2022-11-21 11:41:14 -0500

    Created functionality to get post list from local memory repository.

--- src/main/java/ca/utoronto/fitbook/adapter/persistence/localmemory/PostLocalMemoryRepository.java
+++ src/main/java/ca/utoronto/fitbook/adapter/persistence/localmemory/PostLocalMemoryRepository.java
@@ -2,14 +2,17 @@ package ca.utoronto.fitbook.adapter.persistence.localmemory;
 
 import ca.utoronto.fitbook.adapter.persistence.GenericRepository;
 import ca.utoronto.fitbook.application.port.in.EntityNotFoundException;
+import ca.utoronto.fitbook.application.port.in.LoadPostListPort;
 import ca.utoronto.fitbook.application.port.in.LoadPostPort;
 import ca.utoronto.fitbook.application.port.out.SavePostPort;
 import ca.utoronto.fitbook.entity.Post;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class PostLocalMemoryRepository implements GenericRepository<Post>, LoadPostPort, SavePostPort
+public class PostLocalMemoryRepository implements GenericRepository<Post>, LoadPostPort, SavePostPort, LoadPostListPort
 {
     private static final Map<String, Post> datastore = new HashMap<>();
 
@@ -48,4 +51,19 @@ public class PostLocalMemoryRepository implements GenericRepository<Post>, LoadP
     public void savePost(Post post) {
         save(post);
     }
+
+    /**
+     * @param postIds The post ids to be fetched
+     * @return A list of posts
+     * @throws EntityNotFoundException If a single post is not found
+     */
+    @Override
+    public List<Post> loadPostList(List<String> postIds) throws EntityNotFoundException {
+        List<Post> postList = new ArrayList<>();
+        for (String id : postIds) {
+            postList.add(getById(id));
+        }
+        return postList;
+    }
 }
+
