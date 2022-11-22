540761132e52afbc0cc95c706811fb0a3e19729c
Xavier Ibanez-Padron <xavier.ibanezpadron@mail.utoronto.ca>
2022-11-21 23:25:52 -0500

    post creation use case -- added checks for exercise id's

---
+++ src/main/java/ca/utoronto/fitbook/application/port/in/exception/ExerciseInListNotFoundException.java
@@ -0,0 +1,11 @@
+package ca.utoronto.fitbook.application.port.in.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.UNPROCESSABLE_ENTITY, reason="Entity not found")
+public class ExerciseInListNotFoundException extends RuntimeException {
+    public ExerciseInListNotFoundException() {
+        super("Some exercise in list not found.");
+    }
+}
--- src/main/java/ca/utoronto/fitbook/application/port/in/exception/UserNotFoundException.java
+++ src/main/java/ca/utoronto/fitbook/application/port/in/exception/UserNotFoundException.java
@@ -3,7 +3,7 @@ package ca.utoronto.fitbook.application.port.in.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="User not found")
+@ResponseStatus(value= HttpStatus.UNPROCESSABLE_ENTITY, reason="User not found")
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String id) {
         super(String.format("User with id %s not found.", (id)));
