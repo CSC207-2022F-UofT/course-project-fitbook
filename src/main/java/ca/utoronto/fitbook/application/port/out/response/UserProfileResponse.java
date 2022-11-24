32c7bdc5bf0d954769c23787701d765b601b2ea1
juanpabloacosta <acosta.jp@icloud.com>
2022-11-24 16:34:31 -0500

    Added non-null annotations

--- src/main/java/ca/utoronto/fitbook/application/port/out/response/UserProfileResponse.java
+++ src/main/java/ca/utoronto/fitbook/application/port/out/response/UserProfileResponse.java
@@ -2,6 +2,7 @@ package ca.utoronto.fitbook.application.port.out.response;
 
 import ca.utoronto.fitbook.entity.Exercise;
 import ca.utoronto.fitbook.entity.Post;
+import lombok.NonNull;
 import lombok.Value;
 
 import java.util.List;
@@ -9,13 +10,19 @@ import java.util.Map;
 
 @Value
 public class UserProfileResponse {
+    @NonNull
     String id;
+    @NonNull
     String name;
     int followingSize;
     int followerSize;
+    @NonNull
     String joinDate;
+    @NonNull
     List<Post> postList;
+    @NonNull
     List<Post> likedPostList;
+    @NonNull
     Map<String, Exercise> userExercises;
-
+    int totalLikes;
 }
