628e4ed77590e2961a8243e5a20179ac6e047def
youssefsoli <youssefdevelops@gmail.com>
2022-11-25 18:23:48 -0500

    Renamed userName to name

--- src/main/java/ca/utoronto/fitbook/application/port/in/FindUserByNamePort.java
+++ src/main/java/ca/utoronto/fitbook/application/port/in/FindUserByNamePort.java
@@ -1,7 +1,5 @@
 package ca.utoronto.fitbook.application.port.in;
 
-import ca.utoronto.fitbook.entity.User;
-
 public interface FindUserByNamePort {
-    boolean findByName(String userName);
+    boolean findByName(String name);
 }
