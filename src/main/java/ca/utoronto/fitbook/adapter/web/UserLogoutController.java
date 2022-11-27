c615f3fda49042f67b930f439f2ad95353ef8271
youssefsoli <youssefdevelops@gmail.com>
2022-11-27 03:05:45 -0500

    Add user logout controller

---
+++ src/main/java/ca/utoronto/fitbook/adapter/web/UserLogoutController.java
@@ -0,0 +1,20 @@
+package ca.utoronto.fitbook.adapter.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequiredArgsConstructor
+public class UserLogoutController {
+    @GetMapping(path = "/logout")
+    String logoutUser(Model model, HttpSession session) {
+        session.invalidate();
+
+        // TODO: Show a logout view
+        return "index";
+    }
+}
