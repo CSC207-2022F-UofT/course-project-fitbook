defea63200c95086c96478d583efe4e79f949db2
youssefsoli <youssefdevelops@gmail.com>
2022-11-27 03:06:15 -0500

    Use ID in user session for follow use case

--- src/main/java/ca/utoronto/fitbook/adapter/web/FollowController.java
+++ src/main/java/ca/utoronto/fitbook/adapter/web/FollowController.java
@@ -1,5 +1,6 @@
 package ca.utoronto.fitbook.adapter.web;
 
+import ca.utoronto.fitbook.adapter.persistence.UnauthorizedUserException;
 import ca.utoronto.fitbook.application.port.in.FollowUseCase;
 import ca.utoronto.fitbook.application.port.in.command.FollowCommand;
 import ca.utoronto.fitbook.application.port.out.response.FollowResponse;
@@ -9,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class FollowController
@@ -16,9 +19,15 @@ public class FollowController
     private final FollowUseCase followUseCase;
 
     @PostMapping(path = "/follow")
-    String followUser(Model model, @RequestBody FollowCommand command) {
+    String followUser(Model model, HttpSession session, @RequestBody FollowRequestBody body) {
+        String userId = (String) session.getAttribute("userId");
+        if (userId == null)
+            throw new UnauthorizedUserException();
+        FollowCommand command = new FollowCommand(userId, body.getFolloweeId());
         FollowResponse response = followUseCase.followUser(command);
         model.addAttribute("id", response.getFolloweeId());
+
+        // TODO: Show a follow success view
         return "index";
     }
 }
