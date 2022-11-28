92ead852b9f1e65cf562d1a2d5c84d3ff5fe0468
Xavier Ibanez-Padron <xavier.ibanezpadron@mail.utoronto.ca>
2022-11-28 10:43:20 -0500

    Changed exception error reason for clarity

--- src/main/java/ca/utoronto/fitbook/application/port/in/ExerciseInListNotFoundException.java
+++ src/main/java/ca/utoronto/fitbook/application/port/in/ExerciseInListNotFoundException.java
@@ -3,7 +3,7 @@ package ca.utoronto.fitbook.application.port.in;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.UNPROCESSABLE_ENTITY, reason="Entity not found")
+@ResponseStatus(value= HttpStatus.UNPROCESSABLE_ENTITY, reason="Some exercise in list does not exist")
 public class ExerciseInListNotFoundException extends RuntimeException {
     public ExerciseInListNotFoundException() {
         super("Some exercise in list not found.");
