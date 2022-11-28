5c999b5c8243ede5f109dec80bc13938f71e68d9
Modaser Mojadiddi <modaser.farooqi@gmail.com>
2022-11-28 11:58:06 -0500

    Modified the controller for login and signup to properly execute the requests for post and get. Created the ui files for login and signup.

--- src/main/java/ca/utoronto/fitbook/adapter/web/UserRegisterController.java
+++ src/main/java/ca/utoronto/fitbook/adapter/web/UserRegisterController.java
@@ -6,6 +6,7 @@ import ca.utoronto.fitbook.application.port.out.response.UserRegisterResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -16,13 +17,18 @@ import javax.servlet.http.HttpSession;
 public class UserRegisterController {
     private final UserRegisterUseCase userRegisterUseCase;
 
-    @PostMapping(path = "/register")
-    String registerUser(Model model, HttpSession session, @RequestBody UserRegisterCommand command) {
+    @PostMapping(path = "/register", consumes = "application/x-www-form-urlencoded")
+    String postRegisterUser(Model model, HttpSession session, UserRegisterCommand command) {
         UserRegisterResponse response = userRegisterUseCase.createUser(command);
         session.setAttribute("userId", response.getId());
         model.addAttribute("id", response.getId());
 
         // TODO: Show a register success view
         return "home";
+    }
+
+    @GetMapping(path = "/register")
+    String getRegisterUser(){
+        return "register";
     }
 }
