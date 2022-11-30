8017ca8836233af823d64dc2030e066a09cddeed
Xavier Ibanez-Padron <98190767+Ludosss@users.noreply.github.com>
2022-11-30 13:06:19 -0500

    Update src/main/java/ca/utoronto/fitbook/application/port/in/UserNotFoundException.java
    changed
    
    Co-authored-by: JacketPants12 <113369778+JacketPants12@users.noreply.github.com>

--- src/main/java/ca/utoronto/fitbook/application/port/in/UserNotFoundException.java
+++ src/main/java/ca/utoronto/fitbook/application/port/in/UserNotFoundException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value= HttpStatus.UNPROCESSABLE_ENTITY, reason="User not found")
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String id) {
-        super(String.format("User with id %s not found.", (id)));
+        super(String.format("User with id %s not found.", id));
     }
 }
