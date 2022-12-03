83559854eb3007c810c5de693dee193de8e00816
juanpabloacosta <acosta.jp@icloud.com>
2022-12-03 02:17:53 -0500

    Added documentation and comments for clarity

--- src/main/java/ca/utoronto/fitbook/adapter/web/UserProfileController.java
+++ src/main/java/ca/utoronto/fitbook/adapter/web/UserProfileController.java
@@ -18,13 +18,26 @@ public class UserProfileController
 {
     private final UserProfileUseCase userProfileUseCase;
 
+    /**
+     * @param model profile display information
+     * @param session current user's web session
+     * @param profileId id of the requested user's profile
+     * @return  user profile view page
+     */
     @GetMapping(path = "/profile/{profileId}")
     String userProfile(Model model, HttpSession session, @PathVariable String profileId) {
+        // Get session's userId, throw exception if null
         String userId = (String) session.getAttribute("userId");
         if (userId == null)
             throw new UnauthorizedUserException();
+
+        // Create input data with session's userId
         UserProfileCommand command = new UserProfileCommand(profileId, userId);
+
+        // Receive output data with use case method
         UserProfileResponse response = userProfileUseCase.findProfile(command);
+
+        // Add profile information to display to model
         model.addAttribute("profile", response);
         model.addAttribute("currUser", userId);
         return "profile";
