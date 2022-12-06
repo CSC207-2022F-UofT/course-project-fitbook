81c4a5aa686b116007eccee5aea5165d7f835fd9
jannater <anis_rahim@yahoo.com>
2022-12-05 20:47:52 -0500

    Refactored FollowController to RestController

--- src/main/java/ca/utoronto/fitbook/adapter/web/FollowController.java
+++ src/main/java/ca/utoronto/fitbook/adapter/web/FollowController.java
@@ -5,30 +5,26 @@ import ca.utoronto.fitbook.application.port.in.FollowUseCase;
 import ca.utoronto.fitbook.application.port.in.command.FollowCommand;
 import ca.utoronto.fitbook.application.port.out.response.FollowResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class FollowController
 {
     private final FollowUseCase followUseCase;
 
     @PostMapping(path = "/follow")
-    String followUser(Model model, HttpSession session, @RequestBody FollowRequestBody body) {
+    FollowResponse followUser(HttpSession session, @RequestBody FollowRequestBody body) {
         String userId = (String) session.getAttribute("userId");
         if (userId == null)
             throw new UnauthorizedUserException();
         FollowCommand command = new FollowCommand(userId, body.getFolloweeId());
-        FollowResponse response = followUseCase.followUser(command);
-        model.addAttribute("id", response.getFolloweeId());
 
-        // TODO: Show a follow success view
-        return "index";
+        return followUseCase.followUser(command);
     }
 }
 
