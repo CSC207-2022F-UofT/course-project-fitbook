b83e0f8782b3ea6b7d8e9db8166ccc8ee298dbe4
feiwun <102565969+feiwun@users.noreply.github.com>
2022-12-07 17:44:46 -0500

    Update UpvotePostsResponse.java

--- src/main/java/ca/utoronto/fitbook/application/port/out/response/UpvotePostsResponse.java
+++ src/main/java/ca/utoronto/fitbook/application/port/out/response/UpvotePostsResponse.java
@@ -1,13 +1,9 @@
 package ca.utoronto.fitbook.application.port.out.response;
 
-import lombok.NonNull;
 import lombok.Value;
 
 @Value
 public class UpvotePostsResponse {
-    @NonNull
-    String postLikerID;
-    @NonNull
-    String postAuthorID;
+
     int postLikes;
 }
