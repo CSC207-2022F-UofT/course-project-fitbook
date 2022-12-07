0a7107f2efcfe85706c7492803d40a0a65041c5a
Xavier Ibanez-Padron <xavier.ibanezpadron@mail.utoronto.ca>
2022-12-07 16:03:16 -0500

    renamed variables for clarity and added exception checking for excesize types

--- src/main/java/ca/utoronto/fitbook/application/service/ViewExerciseInfoService.java
+++ src/main/java/ca/utoronto/fitbook/application/service/ViewExerciseInfoService.java
@@ -1,6 +1,7 @@
 package ca.utoronto.fitbook.application.service;
 
 
+import ca.utoronto.fitbook.application.exceptions.InvalidExerciseTypeException;
 import ca.utoronto.fitbook.application.port.in.LoadAllExercisesPort;
 import ca.utoronto.fitbook.application.port.in.ViewExerciseInfoUseCase;
 import ca.utoronto.fitbook.application.port.out.response.ViewExerciseInfoResponse;
@@ -27,11 +28,13 @@ public class ViewExerciseInfoService implements ViewExerciseInfoUseCase {
         ArrayList<TemporalExercise> temporalExercises = new ArrayList<>();
         ArrayList<RepetitiveExercise> repetitiveExercises = new ArrayList<>();
 
-        for (Exercise exer : exerciseList) {
-            if (exer instanceof TemporalExercise){
-                temporalExercises.add((TemporalExercise) exer);
+        for (Exercise exercise : exerciseList) {
+            if (exercise instanceof TemporalExercise){
+                temporalExercises.add((TemporalExercise) exercise);
+            } else if (exercise instanceof RepetitiveExercise){
+                repetitiveExercises.add((RepetitiveExercise) exercise);
             } else {
-                repetitiveExercises.add((RepetitiveExercise) exer);
+                throw new InvalidExerciseTypeException(exercise.getClass().getName());
             }
         }
         return new ViewExerciseInfoResponse(temporalExercises, repetitiveExercises);
