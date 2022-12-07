706fda451017fc03adf003e41cd9b8695e5ace4f
feiwun <karen26wong@gmail.com>
2022-12-06 22:03:59 -0500

    upvote posts usecase testing

--- src/main/java/ca/utoronto/fitbook/application/service/UpvotePostsService.java
+++ src/main/java/ca/utoronto/fitbook/application/service/UpvotePostsService.java
@@ -8,18 +8,18 @@ import ca.utoronto.fitbook.application.port.out.SavePostPort;
 import ca.utoronto.fitbook.application.port.out.SaveUserPort;
 import ca.utoronto.fitbook.application.port.out.response.UpvotePostsResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import ca.utoronto.fitbook.entity.Post;
 import ca.utoronto.fitbook.entity.User;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Service
 @RequiredArgsConstructor
-public class upvotePostsService implements UpvotePostsUsecase{
+public class UpvotePostsService implements UpvotePostsUsecase{
 
     private final LoadPostPort loadPostPort;
     private final LoadUserPort loadUserPort;
-    private final SavePostPort savePostPort;
-    private final SaveUserPort saveUserPort;
 
     /**
      * @param command the likes coming in from a user liking a post and a post being liked
@@ -32,10 +32,19 @@ public class upvotePostsService implements UpvotePostsUsecase{
         User postAuthor = loadUserPort.loadUser(likedPost.getAuthorId());
 
         userPostLiker.getLikedPostIdList().add(likedPost.getId());
+        likedPost.setLikes(likedPost.getLikes() + 1);
 
         return new UpvotePostsResponse(
                 userPostLiker.getId(),
-                likedPost.getAuthorId(),
+                postAuthor.getId(),
                 likedPost.getLikes());
     }
+
+    @ResponseStatus(value= HttpStatus.BAD_REQUEST, reason = "User has already liked the post.")
+    public static class UserAlreadyLikedException extends RuntimeException {
+        public UserAlreadyLikedException() {
+            super("User has already liked the post.");
+        }
+    }
+
 }
