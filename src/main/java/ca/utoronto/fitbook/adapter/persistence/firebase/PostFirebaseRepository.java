582cc613f0182c56e54bef114ca2244951b73582
youssefsoli <youssefdevelops@gmail.com>
2022-12-07 23:57:56 -0500

    Implemented the generic repository across repos
    This repository brings an asynchronous list load which is much faster than the previous implementation.

--- src/main/java/ca/utoronto/fitbook/adapter/persistence/firebase/PostFirebaseRepository.java
+++ src/main/java/ca/utoronto/fitbook/adapter/persistence/firebase/PostFirebaseRepository.java
@@ -6,11 +6,9 @@ import ca.utoronto.fitbook.application.port.in.LoadPaginatedPosts;
 import ca.utoronto.fitbook.application.port.in.LoadPostListPort;
 import ca.utoronto.fitbook.application.port.in.LoadPostPort;
 import ca.utoronto.fitbook.application.port.out.SavePostPort;
+import ca.utoronto.fitbook.entity.Post;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
-import ca.utoronto.fitbook.entity.Post;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -20,16 +18,27 @@ import java.util.concurrent.ExecutionException;
 
 @Repository
 @RequiredArgsConstructor
-public class PostFirebaseRepository implements GenericRepository<Post>,
+public class PostFirebaseRepository
+        extends GenericFirebaseRepository
+        implements GenericRepository<Post>,
         LoadPostPort,
         SavePostPort,
         LoadPostListPort,
         LoadPaginatedPosts
 {
 
+    private static final String COLLECTION_NAME = "posts";
     private final Firestore firestore;
 
-    private static final String COLLECTION_NAME = "posts";
+    @Override
+    protected Firestore getFirestore() {
+        return firestore;
+    }
+
+    @Override
+    protected String getCollectionName() {
+        return COLLECTION_NAME;
+    }
 
 
     /**
@@ -39,16 +48,7 @@ public class PostFirebaseRepository implements GenericRepository<Post>,
      */
     @Override
     public Post getById(String id) throws EntityNotFoundException {
-        ApiFuture<DocumentSnapshot> future = firestore.collection(COLLECTION_NAME).document(id).get();
-        try {
-            DocumentSnapshot document = future.get();
-            if (document.exists()) {
-                return document.toObject(Post.class);
-            }
-            throw new EntityNotFoundException(id);
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
+        return getDocumentById(id).toObject(Post.class);
     }
 
     /**
@@ -105,8 +105,11 @@ public class PostFirebaseRepository implements GenericRepository<Post>,
     @Override
     public List<Post> loadPostList(List<String> postIds) throws EntityNotFoundException {
         List<Post> postList = new ArrayList<>();
-        for (String id : postIds) {
-            postList.add(getById(id));
+        List<DocumentSnapshot> postDocumentList = getDocumentList(postIds);
+        for (DocumentSnapshot document : postDocumentList) {
+            if (!document.exists())
+                throw new EntityNotFoundException(document.getId());
+            postList.add(document.toObject(Post.class));
         }
         return postList;
     }
