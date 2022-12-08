cfdff923195fd76429478b5f03452a59de4d23b3
Modaser Mojadiddi <modaser.farooqi@gmail.com>
2022-12-08 10:10:20 -0500

    Added password and username constraints check cleint side

--- src/main/java/ca/utoronto/fitbook/adapter/web/UserRegisterController.java
+++ src/main/java/ca/utoronto/fitbook/adapter/web/UserRegisterController.java
@@ -1,24 +1,29 @@
 package ca.utoronto.fitbook.adapter.web;
 
+import ca.utoronto.fitbook.adapter.persistence.firebase.UserFirebaseRepository;
 import ca.utoronto.fitbook.application.port.in.UserRegisterUseCase;
 import ca.utoronto.fitbook.application.port.in.command.UserRegisterCommand;
 import ca.utoronto.fitbook.application.port.out.response.UserRegisterResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
 public class UserRegisterController {
+    @Autowired
+    private UserFirebaseRepository repository;
+
     private final UserRegisterUseCase userRegisterUseCase;
 
     // Passing the user provided info to the use case and returning either error or homePage
-    @PostMapping(path = "/register", consumes = "application/x-www-form-urlencoded")
+    @PostMapping(path = "/register", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     String postRegisterUser(Model model, HttpSession session, UserRegisterCommand command) {
         UserRegisterResponse response = userRegisterUseCase.createUser(command);
         session.setAttribute("userId", response.getId());
