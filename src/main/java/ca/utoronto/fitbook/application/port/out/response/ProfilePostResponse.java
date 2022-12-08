17ce9d4189f1c773f909881b6b1c26c2410ff652
juanpabloacosta <acosta.jp@icloud.com>
2022-12-07 19:45:56 -0500

    Restructured responses to efficiently contain necessary information.
    Repeated information identical to the Post entity is stored in the post object attribute, with new information in extra attributes.

--- src/main/java/ca/utoronto/fitbook/application/port/out/response/ProfilePostResponse.java
+++ src/main/java/ca/utoronto/fitbook/application/port/out/response/ProfilePostResponse.java
@@ -1,8 +1,8 @@
 package ca.utoronto.fitbook.application.port.out.response;
 
+import ca.utoronto.fitbook.entity.Post;
 import ca.utoronto.fitbook.entity.RepetitiveExercise;
 import ca.utoronto.fitbook.entity.TemporalExercise;
-import ca.utoronto.fitbook.entity.User;
 import lombok.*;
 
 import java.util.List;
@@ -10,17 +10,12 @@ import java.util.List;
 @Value
 public class ProfilePostResponse {
     @NonNull
-    String postId;
+    Post post;
     @NonNull
-    User author;
-    int likes;
-    @NonNull
-    String postDate;
+    String authorName;
     @NonNull
     List<RepetitiveExercise> repetitiveExerciseList;
     @NonNull
     List<TemporalExercise> temporalExerciseList;
-    @NonNull
-    String description;
     boolean userLiked;
 }
