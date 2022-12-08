928e0e38ae3cef99d3377cc38162cf77e1cf0484
OrbitalVitamin <balaria.alex@gmail.com>
2022-12-08 10:36:27 -0500

    Add search controller

---
+++ src/main/java/ca/utoronto/fitbook/adapter/web/SearchController.java
@@ -0,0 +1,30 @@
+package ca.utoronto.fitbook.adapter.web;
+
+import ca.utoronto.fitbook.application.port.in.SearchPostsUseCase;
+import ca.utoronto.fitbook.application.port.in.command.SearchCommand;
+import ca.utoronto.fitbook.application.port.out.response.SearchResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequiredArgsConstructor
+public class SearchController {
+    private final SearchPostsUseCase searchUseCase;
+
+    @GetMapping(path = "/search")
+    String search(Model model, HttpSession session, @RequestParam SearchCommand queryString) {
+        String userId = (String) session.getAttribute("userId");
+        if (userId == null)
+            throw new UnauthorizedUserException();
+
+        SearchResponse searchResponse = searchUseCase.search(queryString);
+        model.addAttribute("postResponseList", searchResponse.getPostList());
+        return "search";
+    }
+
+}
