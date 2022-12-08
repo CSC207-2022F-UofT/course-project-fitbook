b1258b1bf12d4f909af1d5a97e5201cdb1895e1a
youssefsoli <youssefdevelops@gmail.com>
2022-12-08 05:58:59 -0500

    Make upvote controller a REST API

--- src/main/java/ca/utoronto/fitbook/adapter/web/UpvotePostsController.java
+++ src/main/java/ca/utoronto/fitbook/adapter/web/UpvotePostsController.java
@@ -1,24 +1,29 @@
 package ca.utoronto.fitbook.adapter.web;
 
-import ca.utoronto.fitbook.application.port.in.command.UpvotePostsCommand;
+import ca.utoronto.fitbook.adapter.web.requestbody.UpvoteRequestBody;
 import ca.utoronto.fitbook.application.port.in.UpvotePostsUsecase;
+import ca.utoronto.fitbook.application.port.in.command.UpvotePostsCommand;
 import ca.utoronto.fitbook.application.port.out.response.UpvotePostsResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.servlet.http.HttpSession;
+
+@RestController
 @RequiredArgsConstructor
-public class UpvotePostsController {
+public class UpvotePostsController
+{
 
     private final UpvotePostsUsecase upvotePostsUsecase;
 
     @PostMapping(path = "/upvote")
-    String upvotePosts(Model model, @RequestBody UpvotePostsCommand command) {
-        UpvotePostsResponse response = upvotePostsUsecase.upvotePost(command);
-        model.addAttribute("likes", response.getPostLikes());
-        return "likes";
+    UpvotePostsResponse upvotePosts(HttpSession session, @RequestBody UpvoteRequestBody body) {
+        String userId = (String) session.getAttribute("userId");
+        if (userId == null)
+            throw new UnauthorizedUserException();
+        UpvotePostsCommand command = new UpvotePostsCommand(body.getPostId(), userId);
+        return upvotePostsUsecase.upvotePost(command);
     }
 }
